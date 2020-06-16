ae9e5904878b7cf9d2c3b7950067418aa571d697
unknown <pnp3737@gmail.com>
2020-06-17 01:12:44 +0900

    0617

--- source/01_CNN.py
+++ source/01_CNN.py
@@ -5,8 +5,8 @@ import os
 import cv2
 import numpy as np
 
-##################################### 이미지 리사이즈
-################ 1. closed eyes resize
+#### 이미지 리사이즈
+# 1) closed eyes resize
 # display image characteristics
 imc_path = './img/close/ce1.jpg' 
 imc = Image.open(imc_path) 
@@ -56,8 +56,7 @@ for (i, new) in enumerate(resized_open) :
     new.save ('{}{}{}'.format('./eyes/o_eyes/oe', i+1, '.jpg'))
 
 
-######################## NUMPY conversion
-# numpy type dataset
+#### NUMPY conversion
 groups_folder_path = './eyes/'     
 categories = ["c_eyes", "o_eyes"]  
 num_classes = len(categories)
@@ -81,31 +80,13 @@ for index, categorie in enumerate(categories) :
 x = np.array(x)
 y = np.array(y)
 
-
-# # x_pred data 
-# x_pred = []
-
-# groups_folder_path = './data/test_face/'     
-
-# image_dir = groups_folder_path +  '/'
-# for path, dirs, files in os.walk(image_dir) :
-#     for filename in files :
-#         print(image_dir + filename)
-#         img = cv2.imread(image_dir+filename)
-#         x_pred.append(img)
-#         # print(x_pred)
-
-# x_pred = np.array(x_pred)
-
 # 64 x 64
 print("x.shape :", x.shape)   # (200, 64, 64, 3)
 print("y.shape :", y.shape)   # (200, 2)
 
-# 100 x 100 
-# print("x.shape :", x.shape)   # (200, 100, 100, 3)
-# print("y.shape :", y.shape)   # (200, 2)
 
 
+# 최종 예측 위한 데이터 준비
 testimg_dir = './img/test/'
 image_w = 64
 image_h = 64
@@ -135,7 +116,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
 
 from keras.models import Sequential, Model
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
@@ -149,9 +129,6 @@ print("x.shape :", x.shape)
 print("y.shape :", y.shape)
 
 
-print("y :", y)
-
-
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size = 0.2, random_state = 77, shuffle = True
@@ -163,133 +140,83 @@ print("y_train.shape :", y_train.shape)  # (160, 2)
 print("y_test.shape :", y_test.shape)    # (40, 2)
 
 
-# 데이터 전처리
-# scaling(하기 전, 다시 2차원으로 reshape 해 줘야 함)
-
-# 64 x 64
-x_train = x_train.reshape(x_train.shape[0], 64*64*3)
-x_test = x_test.reshape(x_test.shape[0], 64*64*3)
-
-# 100 x 100
-# x_train = x_train.reshape(x_train.shape[0], 100*100*3)
-# x_test = x_test.reshape(x_test.shape[0], 100*100*3)
-
-# scaler = MinMaxScaler()
-scaler = StandardScaler()
-
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-
-# print(x_test)
-
-
-# CNN 모델에 맞게 reshape
-# 64 x 64
-x_train = x_train.reshape(x_train.shape[0], 64, 64, 3)
-x_test = x_test.reshape(x_test.shape[0], 64, 64, 3)
-
-# 100 x 100
-# x_train = x_train.reshape(x_train.shape[0] ,100, 100, 3)
-# x_test = x_test.reshape(x_test.shape[0], 100, 100, 3)
-
+x_train = x_train.astype(float)/255
+x_test = x_test.astype(float)/255
 
 print("x_train.shape :", x_train.shape)
 
+
 # 2. 모델 구성
 
-### 함수형 ###
-input1 = Input(shape = (64, 64, 3))
-layer = Conv2D(2, (2, 2))(input1)
-layer = MaxPooling2D(pool_size = 2)(layer)
-layer = Flatten()(layer)
-layer = Dense(10)(layer)
-layer = Dense(90)(layer)
-layer = Dense(70)(layer)
-layer = Dense(50)(layer)
-layer = Dense(30)(layer)
-layer = Dense(20)(layer)
-layer = Dense(10, activation = 'relu')(layer)
-output1 = Dense(2, activation = 'sigmoid')(layer)
+model = Sequential()
 
-model = Model(inputs = input1, outputs = output1) 
+model.add(Conv2D(64, (3, 3), input_shape = (64, 64, 3))) 
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.3))
 
+model.add(Conv2D(128, (3, 3)))
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.3))
+
+model.add(Conv2D(256, (3,3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = 2))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Dropout(0.5))
+model.add(Dense(32))
+model.add(Dense(2, activation = 'sigmoid'))
 model.summary()
 
 
-'''
+
 # 3. 컴파일, 훈련
-es = EarlyStopping(monitor = 'loss', patience = 10, mode = 'auto')
-
-modelpath = './model/{epoch:02d}--{acc:.4f}.hdf5'
-
-
+es = EarlyStopping(monitor = 'val_loss', patience = 10, mode = 'auto')
+modelpath = './model/{epoch:02d}--{val_loss:.4f}.hdf5'
 cp = ModelCheckpoint(filepath = modelpath, monitor = 'val_loss', save_best_only = True, mode = 'auto')
-
 # tb_hist = TensorBoard(log_dir = 'graph', histogram_freq = 0, write_graph = True, write_image = True)
-
 
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['acc'])
 
-# model.fit(x_train, y_train, epochs = 100, batch_size = 10, validation_split = 0.3,verbose = 1)
-
-# es + cp
-# model.fit(x_train, y_train, epochs = 300, batch_size = 10, validation_split = 0.3,verbose = 1, 
-                                                        #    callbacks = [es, cp])
-
-# tb_hist
-hist = model.fit(x_train, y_train, epochs = 70, batch_size = 32, validation_split = 0.3, verbose = 1)
-                                   
-
+hist = model.fit(x_train, y_train, epochs = 32, batch_size = 32, validation_split = 0.3, verbose = 1, callbacks = [cp])
+                                  
 
 # 4. 평가, 예측
 loss, acc = model.evaluate(x_test, y_test, batch_size = 32)
-print("loss :", loss)
-print("acc :", acc)
-
-
+print("LOSS :", loss)
+print("ACC :", acc)
 
 prediction = model.predict(x_pred)
-print(prediction)
-
 prediction = np.argmax(prediction, axis = 1)
-
-print(prediction)
-print(type(prediction))
 
 for i in prediction :
     if i == 0 :
-        print("CLOSE")
-        print()
+        print("눈을 감고 있습니다zZ")
     else :
-        print("OPEN")
-        print()
-
-
-# print(np.argmax(y_pred, axis = 1))
-# print(y_pred)
+        print("눈빛이 살아 있습니다")
 
 
 # 시각화
+
 plt.figure(figsize = (12, 10))
 
 plt.subplot(2, 1, 1)
-plt.plot(hist.history['loss'], marker = '.', c = 'red', label = 'loss')
-plt.plot(hist.history['val_loss'], marker = 'o', c = 'blue', label = 'val_loss')
+plt.plot(hist.history['loss'], marker = 'o', c = 'red', label = 'loss')
+plt.plot(hist.history['val_loss'], marker = 'v', c = 'blue', label = 'val_loss')
 plt.grid()
-plt.title('loss')
+plt.title('Training and Val loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['loss', 'val_loss'], loc = 'upper right')
+plt.legend(['Training loss', 'Val loss'], loc = 'upper right')
 
 plt.subplot(2, 1, 2)
-plt.plot(hist.history['acc'], marker = '*', c = 'purple', label = 'acc' )
+plt.plot(hist.history['acc'], marker = 'o', c = 'purple', label = 'acc' )
 plt.plot(hist.history['val_acc'], marker = 'v', c = 'green', label = 'val_loss')
 plt.grid()
-plt.title('acc')
+plt.title('Training and Val accuracy')
 plt.ylabel('acc')
 plt.xlabel('epoch')
-plt.legend(['acc', 'val_acc'], loc = 'upper right')
+plt.legend(['Training acc', 'Val acc'], loc = 'upper left')
 
 plt.show()
-'''
